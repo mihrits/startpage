a69560d726bbc51ea86e51f55aa985b65d8f6506
mihrits <41431010+mihrits@users.noreply.github.com>
2019-11-03 01:37:58 +0200

    Delete weather.js

--- weather.js
+++
@@ -1,9 +0,0 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?zip=94040,us&units=imperial&APPID=YOURAPPIDHERE').then(function (response) {
-    return response.json();
-})
-    .then(function (myJson) {
-        document.getElementById('t').innerHTML = myJson.weather[0].description;
-        var img = document.createElement("img");
-        img.src = 'http://openweathermap.org/img/w/'+ myJson.weather[0].icon +'.png';
-        document.getElementById('u').appendChild(img);
-    })
